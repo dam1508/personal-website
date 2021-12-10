edc8cd112606974309be8a644d4a9b1b933c3492
Damian Piotrowski <damianp.150898@gmail.com>
2021-12-10 20:55:44 +0100

    Updates

---
+++ precache-manifest.b1c97e64f14bb1479ad04e3d6f5d6558.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac165c67627e869667bea6c20690247b",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "bfea46fcfda325aaab6c",
+    "url": "/personal-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4d67d25335769af43abf",
+    "url": "/personal-website/static/js/2.25ddee99.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/personal-website/static/js/2.25ddee99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c922dbe6f2b320e85d3b",
+    "url": "/personal-website/static/js/3.77ba683a.chunk.js"
+  },
+  {
+    "revision": "bfea46fcfda325aaab6c",
+    "url": "/personal-website/static/js/main.6e668062.chunk.js"
+  },
+  {
+    "revision": "94b31e499dd145293c13",
+    "url": "/personal-website/static/js/runtime-main.9e7f53d0.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/personal-website/static/media/logo.06e73328.svg"
+  }
+]);
